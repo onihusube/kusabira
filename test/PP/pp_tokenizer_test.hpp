03fb12ce1aca0ede1e35d3828d1edd17965a4cae
dokusasako <dokusasako@dokusasakonoiMac.local>
2019-10-25 17:08:12 +0900

    初期構成

---
+++ test/PP/pp_tokenizer_test.hpp
@@ -0,0 +1,6 @@
+#include "PP/pp_tokenizer.hpp"
+
+namespace pp_tokenaizer_test
+{
+
+} // namespace pp_tokenaizer_test
---
+++ test/kusabira_test.cpp
@@ -0,0 +1,5 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+//テスト定義ヘッダをインクルード
+#include "PP/pp_tokenizer_test.hpp"
