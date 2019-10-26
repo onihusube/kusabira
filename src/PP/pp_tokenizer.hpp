96c4e8541773aa6d82d406c3e09c1949c1b7faa4
onihusube <44743040+onihusube@users.noreply.github.com>
2019-10-26 16:37:40 +0900

    ファイル読み込みクラスの実装

--- src/PP/pp_tokenizer.hpp
+++ src/PP/pp_tokenizer.hpp
@@ -1,13 +1,50 @@
 #include <fstream>
+#include <filesystem>
+#include <memory_resource>
 
+namespace kusabira {
+  namespace fs = std::filesystem;
+
+  using u8_pmralloc = std::pmr::polymorphic_allocator<char8_t>;
+
+  inline std::pmr::monotonic_buffer_resource def_mr{1024ul};
+}
 
 namespace kusabira::PP {
 
-  struct tokenizer {
-  };
-
   struct filereader {
+
+    filereader(const fs::path& filepath, std::pmr::memory_resource* mr = &kusabira::def_mr)
+      : m_srcstream{filepath}
+      , m_alloc{mr}
+      , m_buffer{m_alloc}
+    {}
+
+    [[nodiscard]]
+    auto readline() -> std::pmr::u8string {
+
+      if (std::getline(m_srcstream, m_buffer, '\x0a')) {
+        auto first = reinterpret_cast<char8_t *>(m_buffer.data());
+        auto last = first + m_buffer.size();
+
+        return std::pmr::u8string{first, last, m_alloc};
+      } else {
+        return std::pmr::u8string{m_alloc};
+      }
+    }
+
+    explicit operator bool() const noexcept {
+      return bool(m_srcstream);
+    }
+
+  private:
+    std::ifstream m_srcstream;
+    u8_pmralloc m_alloc;
+    std::pmr::string m_buffer;
   };
 
+  struct tokenizer {
+
+  };
 
 } // namespace kusabira::PP
--- test/PP/pp_tokenizer_test.hpp
+++ test/PP/pp_tokenizer_test.hpp
@@ -1,6 +1,86 @@
+#include "doctest/doctest.h"
+
 #include "PP/pp_tokenizer.hpp"
+
+namespace kusabira::test {
+
+  /**
+  * @brief kusabiraのテストファイル用ディレクトリを雑に取得する
+  * @detail /buildで実行された時と、/kusabiraで実行された時で同じディレクトリを取得できるようにするもの
+  * @detail その他変なところでやられると全くうまく動かない
+  * @return kusabiraのテストファイル群があるトップのディレクトリ
+  */
+  [[nodiscard]]
+  inline auto get_testfiles_dir() -> std::filesystem::path {
+    auto current_dir = std::filesystem::current_path();
+    const auto parentdir_name = current_dir.parent_path().filename().string();
+
+    std::filesystem::path result{};
+
+    if (parentdir_name == "kusabira") {
+      result = current_dir.parent_path();
+    } else {
+      result = current_dir;
+    }
+
+    return result / "test/files";
+  }
+}
 
 namespace pp_tokenaizer_test
 {
+  TEST_CASE("filereader test") {
+    auto testdir = kusabira::test::get_testfiles_dir() / "PP";
+
+    CHECK_UNARY(std::filesystem::is_directory(testdir));
+    CHECK_UNARY(std::filesystem::exists(testdir));
+
+    //CRLF改行ファイルのテスト
+    {
+      kusabira::PP::filereader fr{testdir / "reader_crlf.txt"};
+
+      CHECK_UNARY(bool(fr));
+
+      //1行目
+      auto line = fr.readline();
+
+      CHECK_UNARY(line.length() == 5);
+      //文字列末尾にCRコードが残っているはず
+      auto p = line.data() + 4;
+      CHECK_EQ(*p, u8'\x0d');
+
+      //2行目
+      line = fr.readline();
+
+      CHECK_UNARY(line.length() == 43);
+      //文字列末尾にCRコードが残っているはず
+      p = line.data() + 42;
+      CHECK_EQ(*p, u8'\x0d');
+
+      //3行目、最終行、改行なし
+      line = fr.readline();
+      CHECK_UNARY(line.length() == 4);
+    }
+
+    //LF改行ファイルのテスト
+    {
+      kusabira::PP::filereader fr{testdir / "reader_lf.txt"};
+
+      CHECK_UNARY(bool(fr));
+
+      //1行目
+      auto line = fr.readline();
+
+      CHECK_UNARY(line.length() == 4);
+
+      //2行目
+      line = fr.readline();
+      CHECK_UNARY(line.length() == 15);
+
+      //3行目、最終行、改行なし
+      line = fr.readline();
+      CHECK_UNARY(line.length() == 2);
+    }
+  }
 
 } // namespace pp_tokenaizer_test
