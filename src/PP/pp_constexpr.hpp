166c937bfbea30d1f03fb1907a682eb9c7667326
onihusube <44743040+onihusube@users.noreply.github.com>
2021-02-26 18:19:28 +0900

    整数値読み取りの実装中

--- src/PP/pp_constexpr.hpp
+++ src/PP/pp_constexpr.hpp
@@ -12,20 +12,30 @@
 
 namespace kusabira::PP::inline free_func {
 
-  fn decode_integral_ppnumber(std::u8string_view number_str) -> std::pair<std::variant<std::intmax_t, std::uintmax_t, bool>, std::uint8_t> {
+  fn decode_integral_ppnumber(std::u8string_view number_strview) -> std::pair<std::variant<std::intmax_t, std::uintmax_t, bool>, std::uint8_t> {
     constexpr auto npos = std::u8string_view::npos;
     constexpr std::uint8_t signed_integral = 0;
     constexpr std::uint8_t unsigned_integral = 1;
     constexpr std::uint8_t floating_point = 2;
 
+    std::pmr::u8string numstr{ &kusabira::def_mr };
+    numstr.reserve(number_strview.length());
+
+    // 桁区切り'を取り除くとともに、大文字を小文字化しておく
+    for (auto inner_range : number_strview | std::views::split(u8'\'')) {
+      for (char8_t c : inner_range | std::views::transform([](unsigned char ch) -> char8_t { return std::tolower(ch); })) {
+        numstr.push_back(c);
+      }
+    }
+
     // 浮動小数点数判定
     // 16進浮動小数点数には絶対にpが表れる
-    if (auto p = std::ranges::find_if(number_str, [](char8_t c) { return c == u8'.' or c == u8'p' or c == u8'P';}); p != number_str.end()) {
+    if (auto p = std::ranges::find_if(numstr, [](char8_t c) { return c == u8'.' or c == u8'p';}); p != numstr.end()) {
       return std::make_pair(std::variant<std::intmax_t, std::uintmax_t, bool>{std::in_place_index<floating_point>, false}, floating_point);
     }
     // 小数点の現れないタイプの10進浮動小数点数
     // 1E-1とか2e+2とか3E-4flみたいな、絶対にeが表れる
-    if (auto e = std::ranges::find_if(number_str, [](char8_t c) { return c == u8'e' or c == u8'E';}); e != number_str.end()) {
+    if (auto e = std::ranges::find_if(numstr, [](char8_t c) { return c == u8'e';}); e != numstr.end()) {
       return std::make_pair(std::variant<std::intmax_t, std::uintmax_t, bool>{std::in_place_index<floating_point>, false}, floating_point);
     }
 
@@ -33,13 +43,27 @@ namespace kusabira::PP::inline free_func {
 
     // 2 or 8 or 10 or 16進数判定
     std::uint8_t base = 10;
+    const char8_t *start_pos = std::ranges::data(numstr);
 
-    if (number_str.starts_with(u8"0b") or number_str.starts_with(u8"0B")) {
+    if (numstr.starts_with(u8"0b")) {
       base = 2;
-    } else if (number_str.starts_with(u8"0x") or number_str.starts_with(u8"0X")) {
+      start_pos += 2;
+    } else if (numstr.starts_with(u8"0x")) {
       base = 16;
-    } else if (number_str.starts_with(u8"0")) {
+      start_pos += 2;
+    } else if (numstr.starts_with(u8"0")) {
       base = 8;
+      start_pos += 1;
+    }
+
+    // リテラル部を除いた数値の終端を求める
+    char8_t const * const true_end = std::addressof(numstr.back()) + 1;
+    const char8_t *end_pos = true_end;
+
+    if (base == 16) {
+      end_pos = std::ranges::find_if(start_pos, true_end, [](unsigned char ch) -> bool { return std::isxdigit(ch); });
+    } else {
+      end_pos = std::ranges::find_if(start_pos, true_end, [](unsigned char ch) -> bool { return std::isdigit(ch); });
     }
 
     // リテラル判定、ユーザー定義リテラルはエラー
@@ -47,14 +71,24 @@ namespace kusabira::PP::inline free_func {
     // l L
     // ll LL
     // z Z
-    // ul ull uz lu llu
+    // 大文字は考慮しなくてよく、u以外は他とくっつかない
+    // 候補は  u l ll z ul ull uz lu llu zu
     // uが入ってたら符号なし、それ以外は符号付
-    bool is_unsigned = true;
+    bool is_signed = true;
+    std::u8string_view suffix{end_pos, true_end};
 
+    // 空だったら符号付整数
+    if (not empty(suffix)) {
 
-    // 'を取り除く
+    }
 
     // from_charsに叩き込む
+
+    if (is_signed) {
+      std::intmax_t num;
+    } else {
+      std::uintmax_t num;
+    }
 
     // おわり
   }
